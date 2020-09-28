86491d228c0fda122f169c2902f8e5def7a198e2
vbijvoets <v.bijvoets@gmail.com>
2020-09-28 15:39:47 +0200

    initial h2.3

--- h2.php
+++ h2.php
@@ -1,2 +1,23 @@
 <?php
 
+
+//for ($i = 1;$i < 10; $i++) {
+//    echo "<img src=library/h2/aap" . $i.".jpg />";
+//}
+
+for ($i = 1;$i < 10;$i++) {
+    for ($j = 1; $j < $i; $j++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+for($x = 35;$x >= 25;$x--){
+    echo "hoppelepee";
+    $x--;
+}
+
+
+echo "<style>body{
+text-align: center;
+}</style>";
