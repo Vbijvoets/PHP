2323716860424295181c956f789da2d53a43bf6f
vbijvoets <v.bijvoets@gmail.com>
2020-09-28 15:24:05 +0200

    opdrachten h1

--- index.php
+++ index.php
@@ -4,3 +4,26 @@ $b = "die niet werken";
 $c = "10000";
 
 echo $a.". ik heb alleen ".$c." manieren gevonden ".$b.".";
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet.";
+
+
+echo "<br>";
+echo "Mensen ".$a.$b." mak".$b." werken ".$c;
+
+$a = "ry";
+$b = ", but";
+$c = "fail";
+
+echo "<br>";
+echo "T".$a." and ".$c.$b." never ".$c." to t".$a.".";
+
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+
+//De cursus “omgaan met teleurstellingen” kan vanavond helaas niet doorgaan.
+echo "<br>";
+echo "De cursus ". $a ." ". $b ." ".$c."rstellingen\" kan vanavond helaas niet doorgaan.";
