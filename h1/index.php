33e00df4499f6d318a7d13000bcd05c69cb281a9
vbijvoets <v.bijvoets@gmail.com>
2020-09-28 15:56:03 +0200

    changes of directories

---
+++ h1/index.php
@@ -0,0 +1,29 @@
+<?php
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = "10000";
+
+echo $a.". ik heb alleen ".$c." manieren gevonden ".$b.".";
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet.";
+
+
+echo "<br>";
+echo "Mensen ".$a.$b." mak".$b." werken ".$c;
+
+$a = "ry";
+$b = ", but";
+$c = "fail";
+
+echo "<br>";
+echo "T".$a." and ".$c.$b." never ".$c." to t".$a.".";
+
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+
+//De cursus “omgaan met teleurstellingen” kan vanavond helaas niet doorgaan.
+echo "<br>";
+echo "De cursus ". $a ." ". $b ." ".$c."rstellingen\" kan vanavond helaas niet doorgaan.";
